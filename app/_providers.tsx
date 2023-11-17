788f2eb71e1d27a14bc3e5f58d9ac3cc7c82830c
himanshuc11 <himanshuchhatpar@gmail.com>
2023-11-17 10:27:51 +0530

    init: Added react query

---
+++ app/_providers.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+type Props = {
+  children: ReactNode;
+};
+
+function Provider(props: Props) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {props.children}
+    </QueryClientProvider>
+  );
+}
+
+export default Provider;
