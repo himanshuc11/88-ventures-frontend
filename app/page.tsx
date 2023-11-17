bdf881d2ede929d41e7b2edf97712c300717d6e1
himanshuc11 <himanshuchhatpar@gmail.com>
2023-11-17 11:53:08 +0530

    feat: Added sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import Header from "@/app/_components/Header";
+import Sidebar from "@/app/_components/Sidebar";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-white">
+    <main className="flex min-h-screen flex-col items-center bg-white flex-1">
       <Header />
+      <section className="flex flex-1 w-full">
+        <Sidebar />
+      </section>
     </main>
   );
 }
